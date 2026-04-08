98ef1c21ee4edda0bcd3d7adbd152fad0924f350
ivan <ivan@cowork.dev>
2026-04-08 03:17:18 -0700

    remove v1

--- manual-bas-v1/trace_gt3.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('data/3/ground_truth.json') as f:
-    gt = json.load(f)
-for e in gt:
-    fr = round(e['chunk_time_ms'] * 25 / 1000)
-    if 460 <= fr <= 580:
-        print(f"  frame={fr:4d}  ms={e['chunk_time_ms']:6d}  type={e['type']}")
